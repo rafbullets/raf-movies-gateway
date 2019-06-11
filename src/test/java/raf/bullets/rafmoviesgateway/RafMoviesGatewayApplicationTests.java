f29468c3e58f6cbd425e49dd9c0b97d56e155ef5
antic <santic16@raf.rs>
2019-06-11 20:26:32 +0200

    Initial commit

---
+++ src/test/java/raf/bullets/rafmoviesgateway/RafMoviesGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package raf.bullets.rafmoviesgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RafMoviesGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
